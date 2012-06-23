61d8d858d9fd64c3cef2f941fa5ef2b5af802f40
Ross Rowe <piaross@gmail.com>
2012-06-23 16:43:00 +1000

    More refactoring of pom files

--- testng/src/test/java/com/saucelabs/testng/WebDriverTest.java
+++ testng/src/test/java/com/saucelabs/testng/WebDriverTest.java
@@ -3,5 +3,48 @@ package com.saucelabs.testng;
 /**
  * @author Ross Rowe
  */
-public class WebDriverTest {
+
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+import static junit.framework.Assert.assertEquals;
+
+@Listeners({SauceOnDemandTestListener.class})
+public class WebDriverTest implements SauceOnDemandSessionIdProvider {
+
+
+    private WebDriver driver;
+
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabillities = DesiredCapabilities.firefox();
+        capabillities.setCapability("version", "5");
+        capabillities.setCapability("platform", Platform.XP);
+        capabillities.setCapability("name", "xxTest : "+testName.getMethodName());
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabillities);
+    }
+
+    @Override
+    public String getSessionId() {
+        return ((RemoteWebDriver)driver).getSessionId().toString();
+    }
+
+    @Test
+    public void basic() throws Exception {
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 }
