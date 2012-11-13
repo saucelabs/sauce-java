92e7b17e142f7ebe14aafae0e9dbea20e8d77cc9
Ross Rowe <piaross@gmail.com>
2012-11-14 10:21:54 +1100

    Added parallel test which uses java helper

---
+++ junit/src/test/java/com/saucelabs/junit/WebDriverParallelWithHelperTest.java
@@ -0,0 +1,85 @@
+package com.saucelabs.junit;
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.LinkedList;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Ross Rowe
+ */
+public class WebDriverParallelWithHelperTest implements SauceOnDemandSessionIdProvider {
+
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication();
+
+    public
+    @Rule
+    SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    public
+    @Rule
+    TestName testName = new TestName();
+
+    private WebDriver driver;
+    private String sessionId;
+
+    private String browser;
+    private String os;
+    private String version;
+
+    public WebDriverParallelWithHelperTest(String os, String version, String browser) {
+        super();
+        this.os = os;
+        this.version = version;
+        this.browser = browser;
+    }
+
+    @Parameterized.Parameters
+    public static LinkedList browsersStrings() throws Exception {
+        LinkedList browsers = new LinkedList();
+        browsers.add(new String[]{Platform.MAC.toString(), "5.0", "iPhone"});
+        return browsers;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabillities = DesiredCapabilities.firefox();
+        capabillities.setCapability(CapabilityType.BROWSER_NAME, browser);
+        capabillities.setCapability(CapabilityType.VERSION, version);
+        capabillities.setCapability(CapabilityType.PLATFORM, Platform.valueOf(os));
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabillities);
+        this.sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
+    }
+
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    @Test
+    public void basic() throws Exception {
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
