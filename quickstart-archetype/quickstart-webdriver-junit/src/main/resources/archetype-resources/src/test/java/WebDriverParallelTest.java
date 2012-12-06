689b64d0110b0bcbd7e4813490cb6a42ade36448
Ross Rowe <piaross@gmail.com>
2012-12-06 15:38:42 +1100

    Added WebDriverParallelTest to junit archetype, fixes saucelabs/java-tutorial#24

---
+++ quickstart-archetype/quickstart-webdriver-junit/src/main/resources/archetype-resources/src/test/java/WebDriverParallelTest.java
@@ -0,0 +1,71 @@
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Demonstrates how to write a JUnit test that runs tests against Sauce OnDemand in parallel.
+ *
+ * @author Ross Rowe
+ */
+@RunWith(Parallelized.class)
+public class WebDriverParallelTest {
+
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication();
+
+    private String browser;
+    private String os;
+    private String version;
+
+    public WebDriverParallelTest(String os, String version, String browser) {
+        super();
+        this.os = os;
+        this.version = version;
+        this.browser = browser;
+    }
+
+    @Parameterized.Parameters
+    public static LinkedList browsersStrings() throws Exception {
+        LinkedList browsers = new LinkedList();
+        browsers.add(new String[]{Platform.VISTA.toString(), "17", "firefox"});
+        return browsers;
+    }
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabillities = new DesiredCapabilities();
+        capabillities.setCapability(CapabilityType.BROWSER_NAME, browser);
+        capabillities.setCapability(CapabilityType.VERSION, version);
+        capabillities.setCapability(CapabilityType.PLATFORM, Platform.valueOf(os));
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabillities);
+    }
+
+    @Test
+    public void webDriver() throws Exception {
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
