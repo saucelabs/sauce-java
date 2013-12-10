4d9d4ab760faecf1191735ee3c27f4811bb2ebb0
Ross Rowe <piaross@gmail.com>
2013-12-10 20:59:13 +1100

    Added support for additional build number environment variables

--- common/src/main/java/com/saucelabs/common/Utils.java
+++ common/src/main/java/com/saucelabs/common/Utils.java
@@ -10,11 +10,43 @@ public final class Utils {
     private Utils() {
     }
 
+    /**
+     * Populates the <code>updates</code> map with the value of the system property/environment variable
+     * with the following name:
+     * <ol>
+     *     <li>BAMBOO_BUILDNUMBER</li>
+     *     <li>JENKINS_BUILD_NUMBER</li>
+     *     <li>BUILD_TAG</li>
+     *     <li>BUILD_NUMBER</li>
+     *     <li>TRAVIS_BUILD_NUMBER</li>
+     *     <li>CIRCLE_BUILD_NUM</li>
+     * </ol>
+     * @param updates
+     */
     public static void addBuildNumberToUpdate(Map<String, Object> updates) {
+        //try Bamboo
         String buildNumber = readPropertyOrEnv("BAMBOO_BUILDNUMBER", null);
         if (buildNumber == null || buildNumber.equals("")) {
             //try Jenkins
             buildNumber = readPropertyOrEnv("JENKINS_BUILD_NUMBER", null);
+        }
+
+        if (buildNumber == null || buildNumber.equals("")) {
+            //try BUILD_TAG
+            buildNumber = readPropertyOrEnv("BUILD_TAG", null);
+        }
+
+        if (buildNumber == null || buildNumber.equals("")) {
+            //try BUILD_NUMBER
+            buildNumber = readPropertyOrEnv("BUILD_NUMBER", null);
+        }
+        if (buildNumber == null || buildNumber.equals("")) {
+            //try TRAVIS_BUILD_NUMBER
+            buildNumber = readPropertyOrEnv("TRAVIS_BUILD_NUMBER", null);
+        }
+        if (buildNumber == null || buildNumber.equals("")) {
+            //try CIRCLE_BUILD_NUM
+            buildNumber = readPropertyOrEnv("CIRCLE_BUILD_NUM", null);
         }
 
         if (buildNumber != null && !(buildNumber.equals(""))) {
