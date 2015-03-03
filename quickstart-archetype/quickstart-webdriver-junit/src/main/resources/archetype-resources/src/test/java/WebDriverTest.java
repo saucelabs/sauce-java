97601fee64f7b890360c89bf7d20b43374b9dfc3
Ross Rowe <piaross@gmail.com>
2015-03-04 07:51:45 +1100

    Resurrected WebDriverTest for JUnit and TestNG archetypes

---
+++ quickstart-archetype/quickstart-webdriver-junit/src/main/resources/archetype-resources/src/test/java/WebDriverTest.java
@@ -0,0 +1,48 @@
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Simple {@link RemoteWebDriver} test that demonstrates how to run your Selenium tests with <a href="http://saucelabs.com/ondemand">Sauce OnDemand</a>.
+ * *
+ * @author Ross Rowe
+ */
+public class WebDriverTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("version", "17");
+        capabilities.setCapability("platform", Platform.XP);
+        this.driver = new RemoteWebDriver(
+                new URL("http://${suserName}:${accessKey}@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+    }
+
+    @Test
+    public void webDriver() throws Exception {
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}
