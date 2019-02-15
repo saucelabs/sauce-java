6ec2ca541ed1bcb107b12fdaf75d841af64a1830
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-02-15 13:19:46 -0500

    Add new, simple unit test

---
+++ common/src/test/java/com.saucelabs.common/SauceHelperTests.java
@@ -0,0 +1,16 @@
+package com.saucelabs.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class SauceHelperTests {
+    @Test
+    public void shouldInstantiateClient()
+    {
+        SauceHelper sauce = new SauceHelper();
+        assertTrue(sauce != null);
+    }
+}
