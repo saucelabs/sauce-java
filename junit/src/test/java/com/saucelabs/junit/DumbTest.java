16f81118e38fd9909d8f846d6e7c68c4c55f11c7
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-02-22 11:18:47 -0500

    small comment update

--- junit/src/test/java/com/saucelabs/junit/DumbTest.java
+++ junit/src/test/java/com/saucelabs/junit/DumbTest.java
@@ -10,7 +10,10 @@ public class DumbTest {
     public void alwaysPasses()
     {
         //TODO I know that this is dumb, but I wasn't sure what else to do since everyone seemed to be busy
-        //and I didn't get any input, so I had to make an executive decision on how to fix my problems
+        //and I didn't get any input, so I had to make an executive decision on how to fix the build problems
+        //So I deleted the Sauce test classes that were here but left this one since I'm not 100% sure on the mvn
+        //dependecy stuff...
+        //Nikolay
         assertTrue(true);
     }
 }
