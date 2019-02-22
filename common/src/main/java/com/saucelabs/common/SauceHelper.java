1dfe64da2ea6c48775dc85284bf4f455d2c86f6c
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-02-22 15:40:55 -0500

    1st 2 tests for the JS executor

--- common/src/main/java/com/saucelabs/common/SauceHelper.java
+++ common/src/main/java/com/saucelabs/common/SauceHelper.java
@@ -1,4 +1,8 @@
 package com.saucelabs.common;
 
 public class SauceHelper {
+
+    public String getTestResultString(boolean result) {
+        return "sauce:job-result=" + result;
+    }
 }
--- common/src/test/java/com.saucelabs.common/SauceHelperTests.java
+++ common/src/test/java/com.saucelabs.common/SauceHelperTests.java
@@ -6,9 +6,15 @@ import static org.junit.Assert.*;
 
 public class SauceHelperTests {
     @Test
-    public void shouldInstantiateClient()
+    public void shouldReturnPassedForTrueResult()
     {
         SauceHelper sauce = new SauceHelper();
-        assertTrue(sauce != null);
+        assertEquals(sauce.getTestResultString(true), "sauce:job-result=true");
+    }
+    @Test
+    public void shouldReturnFailedForFalseResult()
+    {
+        SauceHelper sauce = new SauceHelper();
+        assertEquals(sauce.getTestResultString(false), "sauce:job-result=false");
     }
 }
