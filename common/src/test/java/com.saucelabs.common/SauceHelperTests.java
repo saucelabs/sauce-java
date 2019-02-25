105c1213f18af990bd4b612ff75c50742f235596
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-02-25 09:27:45 -0500

    changed object to private

--- common/src/test/java/com.saucelabs.common/SauceHelperTests.java
+++ common/src/test/java/com.saucelabs.common/SauceHelperTests.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 
 public class SauceHelperTests {
-    SauceHelper sauceHelper;
+    private SauceHelper sauceHelper;
     @Before
     public void runBeforeEveryTest()
     {
