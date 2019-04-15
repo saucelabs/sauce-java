6bd6407c36c4091b2d0bb9c40c2b7c41f597cb4c
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-04-15 15:21:58 -0400

    Remove test duplication

--- common/src/test/java/com.saucelabs.common/SauceGridTest.java
+++ common/src/test/java/com.saucelabs.common/SauceGridTest.java
@@ -1,27 +1,31 @@
 package com.saucelabs.common;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 
 public class SauceGridTest{
+    SauceRemoteGrid sauceGrid;
+    @Before
+    public void setupBeforeTest()
+    {
+        sauceGrid = new SauceRemoteGrid();
+    }
     @Test
     public void shouldReturnObject()
     {
-        SauceRemoteGrid sauceGrid = new SauceRemoteGrid();
         Assert.assertNotNull(sauceGrid);
     }
     @Test
     public void shouldReturnTestObject()
     {
-        SauceRemoteGrid sauceGrid = new SauceRemoteGrid();
         Assert.assertNotNull(sauceGrid.test);
     }
     @Test
     public void shouldSetDefaultBrowserToChrome()
     {
-        SauceRemoteGrid sauceGrid = new SauceRemoteGrid();
         Assert.assertEquals(Browser.Chrome, sauceGrid.test.getBrowser());
     }
 }
