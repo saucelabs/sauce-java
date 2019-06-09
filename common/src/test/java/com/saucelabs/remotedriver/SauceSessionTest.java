6080f4ed462ba7599b2d38be433cc2ccd957f537
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-06-09 15:57:38 -0400

    refactored to remove duplicate/unecessary code

--- common/src/test/java/com/saucelabs/remotedriver/SauceSessionTest.java
+++ common/src/test/java/com/saucelabs/remotedriver/SauceSessionTest.java
@@ -1,6 +1,7 @@
 package com.saucelabs.remotedriver;
 
 import org.hamcrest.text.IsEqualIgnoringCase;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.Capabilities;
@@ -18,76 +19,46 @@ public class SauceSessionTest {
 
     private SauceSession sauceSession;
 
-    @Test
-    public void defaultConstructor_instantiated_returnsFactory()
+    @Before
+    public void setUp()
     {
         sauceSession = new SauceSession();
-        assertNotNull(sauceSession);
     }
-
     @Test
     public void seleniumServer_notSet_returnsNull()
     {
         //TODO is this okay to be like a property,
         //or should it be a getSeleniumServer()
-        sauceSession = new SauceSession();
         assertNull(sauceSession.seleniumServer);
     }
 
     @Test
     public void defaultConstructor_instantiated_setsConcreteDriverManager()
     {
-        sauceSession = new SauceSession();
         assertThat(sauceSession.getDriverManager(), instanceOf(ConcreteRemoteDriverManager.class));
     }
 
     @Test
     public void getInstance_serverNotSet_setsSauceSeleniumServer() throws MalformedURLException {
-        sauceSession = getSessionWithFakeDriverInstance();
-        sauceSession.getInstance();
+        RemoteDriverInterface stubRemoteManager = mock(RemoteDriverInterface.class);
+        sauceSession = new SauceSession(stubRemoteManager);
+        sauceSession.getInstanceOld();
         String expectedServer = "https://ondemand.saucelabs.com/wd/hub";
         assertEquals(expectedServer, sauceSession.seleniumServer);
     }
     @Test
-    @Ignore("old")
-    public void getInstance_default_setsCapabilitiesToChrome() throws MalformedURLException {
-        sauceSession = getSessionWithFakeDriverInstance();
-
-        sauceSession.getInstance();
-
-        String actualBrowser = sauceSession.capabilities.getBrowserName();
-        assertThat(actualBrowser, IsEqualIgnoringCase.equalToIgnoringCase("chrome"));
-    }
-    @Test
     public void noSauceOptionsSet_whenCreated_defaultIsChrome()
     {
-        sauceSession = new SauceSession();
         String actualBrowser = sauceSession.getCapabilities().getBrowserName();
         assertThat(actualBrowser, IsEqualIgnoringCase.equalToIgnoringCase("Chrome"));
     }
     @Test
-    @Ignore("old")
-    public void getInstance_default_setsCapabilityToLinux() throws MalformedURLException {
-        sauceSession = getSessionWithFakeDriverInstance();
-
-        sauceSession.getInstance();
-
-        String actualBrowser = sauceSession.capabilities.getPlatform().name();
-        assertThat(actualBrowser, IsEqualIgnoringCase.equalToIgnoringCase("win10"));
-    }
-    @Test
-    public void noSauceOptionsSet_whenCreated_defaultIsWindows10() throws MalformedURLException {
-        sauceSession = getSessionWithFakeDriverInstance();
-        sauceSession.getInstanceOld();
-        String actualOs = sauceSession.capabilities.getPlatform().name();
+    public void noSauceOptionsSet_whenCreated_defaultIsWindows10() {
+        String actualOs = sauceSession.getCapabilities().getPlatform().name();
         assertThat(actualOs, IsEqualIgnoringCase.equalToIgnoringCase("win10"));
     }
-    private SauceSession getSessionWithFakeDriverInstance() {
-        RemoteDriverInterface stubRemoteManager = mock(RemoteDriverInterface.class);
-        return new SauceSession(stubRemoteManager);
-    }
     @Test
-    public void sauceOptions_defaultConfiguration_latestBrowserVersion()
+    public void noSauceOptionsSet_whenCreated_latestBrowserVersion()
     {
         MutableCapabilities caps = new SauceSession().getCapabilities();
         String actualOperatingSystem = caps.getCapability("browserVersion").toString();
@@ -96,7 +67,6 @@ public class SauceSessionTest {
     @Test
     public void sauceOptions_defaultConfiguration_setsSauceOptions()
     {
-        sauceSession = new SauceSession();
         sauceSession.getCapabilities();
         boolean hasAccessKey = sauceSession.getSauceOptionsCapability().asMap().containsKey("accessKey");
         assertTrue(hasAccessKey);
