5a4c71dcc18084cd7c46f4b507c5934ab36ebfd9
nadvolod <nikolay.advolodkin@saucelabs.com>
2019-06-13 10:20:34 -0400

    Delete the old start() method that used concrete RemoteDriver, changed it for the testable one

--- common/src/test/java/com/saucelabs/common/unit/SauceJavaScriptStringsTest.java
+++ common/src/test/java/com/saucelabs/common/unit/SauceJavaScriptStringsTest.java
@@ -1,38 +1,14 @@
 package com.saucelabs.common.unit;
 
-<<<<<<< HEAD:common/src/test/java/com/saucelabs/common/unit/SauceJavaScriptStringsTest.java
+import com.saucelabs.common.SauceApi;
 import com.saucelabs.common.SauceJavaScriptStrings;
 import org.junit.Assert;
-=======
->>>>>>> b3926482d56790d2c587b3a965d3c63a2222669a:common/src/test/java/com.saucelabs.common/SauceJavaScriptStringsTest.java
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SauceJavaScriptStringsTest {
 
-<<<<<<< HEAD:common/src/test/java/com/saucelabs/common/unit/SauceJavaScriptStringsTest.java
-=======
-    @Test
-    public void shouldReturnCorrectCommandForTrueResult()
-    {
-        sauceHelper = new SauceHelper();
-        Boolean testStatus = true;
-        String actualTestResultCommand = sauceHelper.getTestResultString(testStatus);
-
-        assertEquals("sauce:job-result=true", actualTestResultCommand);
-    }
-
-    @Test
-    public void shouldReturnCorrectCommandForFalseResult()
-    {
-        sauceHelper = new SauceHelper();
-        Boolean testStatus = false;
-
-        String actualTestResultCommand = sauceHelper.getTestResultString(testStatus);
-        assertEquals("sauce:job-result=false", actualTestResultCommand);
-    }
->>>>>>> b3926482d56790d2c587b3a965d3c63a2222669a:common/src/test/java/com.saucelabs.common/SauceJavaScriptStringsTest.java
     @Test
     public void shouldBeCorrectTestNamePrefix()
     {
